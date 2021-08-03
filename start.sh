7f143415ee3a3c899e93b1e4025be77a4dd13842
subinps57 <76514033+subinps57@users.noreply.github.com>
2021-08-03 20:24:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/subinps57/MusicPlayer.git /MusicPlayer
+git clone https://github.com/subinps/MusicPlayer.git /MusicPlayer
 cd /MusicPlayer
 echo "Starting Bot...."
 python3 main.py
